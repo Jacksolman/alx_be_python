d44e5f4e740b07773c56aa05dc530851a2be6a19
Jako solomon <jacksolman1@gmail.com>
2025-05-26 17:08:22 +0100

    Initial commit of ALX Python exercises

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,12 @@
+# hours_to_seconds.py
+
+# Define the variable 'hours' and assign it a value
+hours = 2
+
+# Calculate the number of seconds in the given hours
+# Remember: 60 minutes in an hour, 60 seconds in a minute
+# So, 60 * 60 = 3600 seconds in an hour
+seconds = hours * 3600
+
+# Print the result in the format: [hours] hour(s) is [seconds] seconds.
+print(f"{hours} hour(s) is {seconds} seconds.")
